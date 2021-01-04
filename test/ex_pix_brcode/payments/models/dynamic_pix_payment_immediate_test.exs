7dff05fc229e4eaacdbe303fc7920d3af4b3a0a6
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2021-01-04 20:30:22 -0300

    fix: 'devedor' validation
    Only when there is either cpf or cnpj that name is mandatory. In that case, there are participants
    that are using this object with all keys 'null'.

--- test/ex_pix_brcode/payments/models/dynamic_pix_payment_immediate_test.exs
+++ test/ex_pix_brcode/payments/models/dynamic_pix_payment_immediate_test.exs
@@ -5,6 +5,27 @@ defmodule ExPixBRCode.Payments.Models.DynamicImmediatePixPaymentTest do
   alias ExPixBRCode.Payments.Models.DynamicImmediatePixPayment
 
   describe "changeset/2" do
+    test "only validate 'devedor.nome' if cpf or cnpj is present" do
+      payload = %{
+        "calendario" => %{
+          "apresentacao" => "2020-11-25T18:06:39Z",
+          "criacao" => "2020-11-13T13:46:43Z",
+          "expiracao" => 3600
+        },
+        "chave" => "9463d2b0-2b1a-4157-80fe-344ccf0f7e13",
+        "devedor" => %{"cpf" => nil, "cnpj" => nil, "nome" => nil},
+        "infoAdicionais" => [%{"nome" => "Campo", "valor" => "Valor"}],
+        "revisao" => 0,
+        "solicitacaoPagador" => "Solicitação",
+        "status" => "ATIVA",
+        "txid" => "123456789012345678901234567890",
+        "valor" => %{"original" => "1.30"}
+      }
+
+      assert {:ok, %DynamicImmediatePixPayment{}} =
+               Changesets.cast_and_apply(DynamicImmediatePixPayment, payload)
+    end
+
     test "successfully validates a proper payload" do
       payload = %{
         "calendario" => %{
