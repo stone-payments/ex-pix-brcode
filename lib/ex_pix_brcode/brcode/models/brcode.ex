7d1e25e5184898812addfc77694a37b9b2b78702
João Gularte <38814531+joaogularte@users.noreply.github.com>
2021-02-15 13:54:55 -0300

    refactor: Accept more valid formats for transaction_amount field (#9)
    * refactor: accept free text values on reference_label
    
    * refactor: validate field length
    
    * chore: pass right argument
    
    * test: redacted fields
    
    * refactor: change regex under transacion_amount field
    
    * test: validate new regex
    
    * refactor: change regex to accept more formats
    
    * chore: remove spec

--- lib/ex_pix_brcode/brcode/models/brcode.ex
+++ lib/ex_pix_brcode/brcode/models/brcode.ex
@@ -77,11 +77,8 @@ defmodule ExPixBRCode.BRCodes.Models.BRCode do
     |> validate_format(:merchant_category_code, ~r/^[0-9]{4}$/)
     |> validate_inclusion(:transaction_currency, ~w(986))
     |> validate_length(:transaction_amount, max: 13)
-    # Formats accept: "0", "0.10", ".10", "1.", "1", "123.99", "123456789.23"
-    |> validate_format(
-      :transaction_amount,
-      ~r/(^0$)|(^[0-9]+\.[0-9]{2}$)|(^[1-9]{1}([0-9])*(\.)?$)|(^\.[0-9]{2}$)/
-    )
+    # Formats accept: "0", "0.10", ".10", "1.", "1", "123.9","123.99", "123456789.23"
+    |> validate_format(:transaction_amount, ~r/^0$|^[0-9]+\.[0-9]{2}$|^[0-9]+\.[0-9]{1}$|^[1-9]{1}[0-9]*\.?$|^\.[0-9]{2}$/)
     |> validate_inclusion(:country_code, ~w(BR))
     |> validate_length(:postal_code, is: 8)
     |> put_type()
