9793f9ed1c253e1be20fb87cb4a0bc8bfa979b07
danielkv7 <danielkv7@gmail.com>
2021-11-30 19:17:50 -0300

    feat: add pix change and pix withdrawal changeset

--- lib/ex_pix_brcode/payments/models/dynamic_immediate_pix_payment.ex
+++ lib/ex_pix_brcode/payments/models/dynamic_immediate_pix_payment.ex
@@ -16,7 +16,13 @@ defmodule ExPixBRCode.Payments.Models.DynamicImmediatePixPayment do
   @calendario_optional [:expiracao]
 
   @valor_required [:original]
-  @valor_optional []
+  @valor_optional [:modalidadeAlteracao]
+
+  @saque_required [:valor, :prestadorDoServicoDeSaque, :modalidadeAgente]
+  @saque_optional [:modalidadeAlteracao]
+
+  @troco_required [:valor, :prestadorDoServicoDeSaque, :modalidadeAgente]
+  @troco_optional [:modalidadeAlteracao]
 
   embedded_schema do
     field :revisao, :integer
@@ -41,7 +47,62 @@ defmodule ExPixBRCode.Payments.Models.DynamicImmediatePixPayment do
     end
 
     embeds_one :valor, Valor, primary_key: false do
+      # Campo Obrigatório
+      # Quando NAO for saque ou troco, DEVE ser maior que 0 EXCETO se modalidadeAlteracao for 1
+      # Quando for saque, DEVE ser 0
+      # Quando for troco, DEVE ser MAIOR que 0 referente a compra do produto.
       field :original, :decimal
+
+      # Campo opcional
+      # Valores aceitos serão 0 ou 1
+      # Caso ausente, o valor será 0
+      # Caso seja saque ou troco, sempre será 0 (ou pode vir sem prenchimento que no caso será igual a 0)
+      field :modalidadeAlteracao, :integer, default: 0
+
+      embeds_one :retirada, Retirada, primary_key: false do
+        # valor.retirada.saque  e valor.retirada.troco não é possível que ambas sejam fornecidas em conjunto (São mutualmente excludentes)
+        # Campo opcional
+        embeds_one :saque, Saque, primary_key: false do
+          # CAMPO OBRIGATÕRIO
+          # Deve ser maior que 0 se valor.retirada.saque.modalidadeAlteracao for 0(zero)
+          # Pode ser 0 caso valor.retirada.saque.modalidadeAlteracao for 1
+          field :valor, :string
+
+          # Campo opcional
+          # Valores aceitos serão 0 ou 1
+          # Caso ausente, o valor será 0
+          field :modalidadeAlteracao, :integer, default: 0
+
+          # CAMPO OBRIGATÕRIO
+          # Adicionar o validação de ISPB
+          field :prestadorDoServicoDeSaque, :string
+
+          # CAMPO OBRIGATÕRIO
+          # Valores que sao aceitos (AGTEC) (AGTOT) (AGPSS)
+          field :modalidadeAgente, :string
+        end
+
+        # Campo opcional
+        embeds_one :troco, Troco, primary_key: false do
+          # CAMPO OBRIGATÕRIO
+          # DEVE ser maior que 0 caso valor.retirada.troco.modalidadeAlteracao for 0 (zero)
+          # Pode ser 0.00 (zero) se valor.retirada.troco.modalidadeAlteracao for 1
+          field :valor, :string
+
+          # Campo opcional
+          # Valores aceitos serão 0 ou 1
+          # Caso ausente, o valor será 0
+          field :modalidadeAlteracao, :integer, default: 0
+
+          # CAMPO OBRIGATÕRIO
+          # Adicionar o validação de ISPB
+          field :prestadorDoServicoDeSaque, :string
+
+          # CAMPO OBRIGATÕRIO
+          # Valor aceito (AGTEC)
+          field :modalidadeAgente, :string
+        end
+      end
     end
 
     embeds_many :infoAdicionais, InfoAdicionais, primary_key: false do
@@ -76,19 +137,6 @@ defmodule ExPixBRCode.Payments.Models.DynamicImmediatePixPayment do
     |> validate_required(@calendario_required)
   end
 
-  defp info_adicionais_changeset(model, params) do
-    model
-    |> cast(params, [:nome, :valor])
-    |> validate_required([:nome, :valor])
-  end
-
-  defp valor_changeset(model, params) do
-    model
-    |> cast(params, @valor_required ++ @valor_optional)
-    |> validate_required(@valor_required)
-    |> validate_number(:original, greater_than: 0)
-  end
-
   defp devedor_changeset(model, params) do
     model
     |> cast(params, [:nome, :cpf, :cnpj])
@@ -115,5 +163,115 @@ defmodule ExPixBRCode.Payments.Models.DynamicImmediatePixPayment do
       true ->
         Changesets.validate_document(changeset, :cnpj)
     end
+  end
+
+  defp valor_changeset(model, params) do
+    model
+    |> cast(params, @valor_required ++ @valor_optional)
+    |> validate_required(@valor_required)
+    |> validate_inclusion(:modalidadeAlteracao, [0, 1])
+    |> validate_valor_original()
+    |> validate_either_saque_or_troco()
+  end
+
+  defp validate_valor_original(changeset) do
+    modalidade_alteracao = get_field(changeset, :modalidadeAlteracao)
+    retirada = get_field(changeset, :retirada)
+
+    if not is_nil(retirada) do
+      saque = get_field(retirada, :saque)
+      troco = get_field(retirada, :troco)
+
+      cond do
+        is_nil(saque) and is_nil(troco) and modalidade_alteracao == 0 ->
+          validate_number(changeset, :original, greater_than: 0)
+
+        is_nil(saque) and is_nil(troco) and modalidade_alteracao == 1 ->
+          validate_number(changeset, :original, greater_than_or_equal_to: 0)
+
+        not is_nil(saque) and is_nil(troco) ->
+          validate_number(changeset, :original, equals_to: 0)
+
+        is_nil(saque) and not is_nil(troco) ->
+          validate_number(changeset, :original, greater_than: 0)
+      end
+    else
+      cond do
+        modalidade_alteracao == 0 ->
+          validate_number(changeset, :original, greater_than: 0)
+
+        modalidade_alteracao == 1 ->
+          validate_number(changeset, :original, greater_than_or_equal_to: 0)
+      end
+    end
+  end
+
+  defp validate_either_saque_or_troco(changeset) do
+    modalidade_alteracao = get_field(changeset, :modalidadeAlteracao)
+    retirada = get_field(changeset, :retirada)
+
+    if not is_nil(retirada) do
+      saque = get_field(retirada, :saque)
+      troco = get_field(retirada, :troco)
+
+      cond do
+        is_nil(saque) and is_nil(troco) ->
+          changeset
+
+        not is_nil(saque) and not is_nil(troco) ->
+          add_error(changeset, :retirada, "only one of saque or troco must be present")
+
+        modalidade_alteracao == 1 ->
+          add_error(changeset, :modalidadeAlteracao, "must be 0 when it is troco or saque")
+
+        not is_nil(saque) ->
+          cast_embed(changeset, :retirada, with: &saque_changeset/2, required: true)
+
+        not is_nil(troco) ->
+          cast_embed(changeset, :retirada, with: &troco_changeset/2, required: true)
+      end
+    else
+      changeset
+    end
+  end
+
+  defp saque_changeset(model, params) do
+    model.saque
+    |> cast(params, @saque_required ++ @saque_optional)
+    |> validate_required(@saque_required)
+    |> validate_inclusion(:modalidadeAlteracao, [0, 1])
+    |> validate_valor()
+    |> validate_length(:prestadorDoServicoDeSaque, is: 8)
+    |> validate_format(:prestadorDoServicoDeSaque, ~r/^[[:digit:]]+$/)
+    |> validate_inclusion(:modalidadeAgente, ["AGTEC", "AGTOT", "AGPSS"])
+  end
+
+  defp troco_changeset(model, params) do
+    model.troco
+    |> cast(params, @troco_required ++ @troco_optional)
+    |> validate_required(@troco_required)
+    |> validate_inclusion(:modalidadeAlteracao, [0, 1])
+    |> validate_valor()
+    |> validate_length(:prestadorDoServicoDeSaque, is: 8)
+    |> validate_format(:prestadorDoServicoDeSaque, ~r/^[[:digit:]]+$/)
+    |> validate_inclusion(:modalidadeAgente, ["AGTEC"])
+  end
+
+  defp validate_valor(changeset) do
+    modalidade_alteracao = get_field(changeset, :modalidadeAlteracao)
+
+    cond do
+      modalidade_alteracao == 0 ->
+        validate_number(changeset, :original, greater_than: 0)
+
+      modalidade_alteracao == 1 ->
+        validate_number(changeset, :original, greater_than_or_equal_to: 0)
+    end
+  end
+
+  defp info_adicionais_changeset(model, params) do
+    model
+    |> cast(params, [:nome, :valor])
+    |> validate_required([:nome, :valor])
   end
 end
