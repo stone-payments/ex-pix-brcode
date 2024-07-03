bc8df522827c433e984daa9dc68e218417b561de
Bruna Leão <52352147+brvnaleao@users.noreply.github.com>
2024-07-03 11:50:35 -0300

    fix: support infoadicionais fields as empty strings for immediate pix (#29)

--- test/ex_pix_brcode/payments/models/dynamic_pix_payment_immediate_test.exs
+++ test/ex_pix_brcode/payments/models/dynamic_pix_payment_immediate_test.exs
@@ -47,6 +47,27 @@ defmodule ExPixBRCode.Payments.Models.DynamicImmediatePixPaymentTest do
                Changesets.cast_and_apply(DynamicImmediatePixPayment, payload)
     end
 
+    test "successfully validates a proper payload  with infoAddicionais as empty strings" do
+      payload = %{
+        "calendario" => %{
+          "apresentacao" => "2020-11-25T18:06:39Z",
+          "criacao" => "2020-11-13T13:46:43Z",
+          "expiracao" => 3600
+        },
+        "chave" => "9463d2b0-2b1a-4157-80fe-344ccf0f7e13",
+        "devedor" => %{"cpf" => Brcpfcnpj.cpf_generate(), "nome" => "Fulano"},
+        "infoAdicionais" => [%{"nome" => "", "valor" => ""}],
+        "revisao" => 0,
+        "solicitacaoPagador" => "Solicitação",
+        "status" => "ATIVA",
+        "txid" => "123456789012345678901234567890",
+        "valor" => %{"original" => "1.30"}
+      }
+
+      assert {:ok, %DynamicImmediatePixPayment{}} =
+               Changesets.cast_and_apply(DynamicImmediatePixPayment, payload)
+    end
+
     test "successfully validates a proper payload without optional fields" do
       payload = %{
         "calendario" => %{
